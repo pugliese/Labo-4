6b59067a8e583e6590025f1c30cf8ef20bdcab2e
pugliese <facu_pugliese@hotmail.com>
2017-02-15 16:53:48 -0300

    La concha de su madre

--- Ferromagnetismo y Temperatura de Curie/Mediciones/Temperatura.m
+++ Ferromagnetismo y Temperatura de Curie/Mediciones/Temperatura.m
@@ -1,28 +1,31 @@
 1;
-Tabla = dlmread("TempVsRes.txt", " ");
-
-
 function res = Temp(V)  % Transforma el voltaje en temperatura
-  i = 1;
+  Tabla = dlmread("TempVsRes.txt", " ");
   res = zeros(1,length(V))-1;
   for k=1:length(V)
-    while Tabla(i,2)<1000*V(k)<Tabla(i+1,2) && i<=rows(Tabla)  % La resistencia es V/1mA
-      i++;                                              % y busco ensangucharla para 
-    endwhile                                            % ubicar la fila
-    j=1;
-    if i>rows(Tabla)        % Nos pasamos de rango y el valor no está en la tabla
-      disp("Voltaje fuera de rango")
+    i=1;
+    if V(k)==0.1
+      res(k) = 0;
     else
-      while Tabla(i,j)<=1000*V(k)<=Tabla(i+1,j) || Tabla(i,j)=>1000*V(k)=>Tabla(i+1,j)   
-        j++;                   % Ahora busco la columna y encuentro la posición                           
-      endwhile
-      if abs(Tabla(i+1,j)-1000*V(k))<abs(1000*V(k)-Tabla(i,j))
-        i++;        % Elijo el valor más cercano
-      endif
-      if i<20
-        res = 10*(i-20)-j;
+      while i<rows(Tabla) && not(Tabla(i,2)<1000*V(k) && 1000*V(k)<Tabla(i+1,2))   % La resistencia es V/1mA
+        i++;                                              % y busco ensangucharla para 
+      endwhile                                            % ubicar la fila
+      j=2;
+      if i>rows(Tabla)        % Nos pasamos de rango y el valor no está en la tabla
+        disp("Voltaje fuera de rango")
       else
-        res = 10*(i-20)+j;
+        while j<columns(Tabla) && not(Tabla(i,j)<=1000*V(k) && 1000*V(k)<=Tabla(i,j+1) || (Tabla(i,j)>=1000*V(k) && 1000*V(k)>=Tabla(i,j+1)))
+          j++;                   % Ahora busco la columna y encuentro la posición                           
+        endwhile
+        if j<columns(Tabla) && abs(Tabla(i,j+1)-1000*V(k))<abs(1000*V(k)-Tabla(i,j))
+          j++;        % Elijo el valor más cercano
+        endif
+        i,j
+        if i<20
+          res(k) = 10*(i-20)-(j-2);
+        else
+          res(k) = 10*(i-21)+(j-2);
+        endif
       endif
     endif
   endfor

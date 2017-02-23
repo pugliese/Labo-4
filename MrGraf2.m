a1f4db311b5c2870c1fc51d7497b1a8decdff216
GonzaloAndreu <gon_e13@hotmail.com>
2017-02-22 23:43:01 -0300

    aca ta, tiene cambiadas un par de boludeces nomas

---
+++ MrGraf2.m
@@ -0,0 +1,28 @@
+function [Mr,Tr]=MrGraf2(t,H,M,f);
+  L = length(H);
+  N = floor(2*180*f); % Cantidad de ceros (aprox el doble de cantidad de ciclos)
+  m = floor(7557/(2*f)); % Cantidad de puntos en un semi-ciclo
+  inf = 1;
+  sup = m+1;    % Me aseguro de enganchar bien el primer 0 en el rango
+  for j=1:N
+    sup = min(sup+floor(3*m/2), L);   % Avanzo un semi-ciclo entero, dandole 
+    inf = min(inf+floor(m/2),sup-1);  % margen a los punteros
+    while inf+1<sup
+      mid = floor((inf+sup)/2);
+      if H(inf)*H(mid)>0  % Tienen el mismo signo, uno sobra
+        inf = mid;
+      else
+        sup = mid;
+      endif
+    endwhile
+    if abs(H(inf))>abs(H(sup))   % Me fijo cual de los 2 está más cerca del 0
+      Mr(j) = M(sup)-(M(sup)-M(inf))*H(sup)/(H(sup)-H(inf));
+      Tr(j) = t(sup)-(t(sup)-t(inf))*H(sup)/(H(sup)-H(inf));
+    else                         % Interpolo con todo el flow
+      Mr(j) = M(inf)-(M(sup)-M(inf))*H(inf)/(H(sup)-H(inf));
+      Tr(j) = t(inf)-(t(sup)-t(inf))*H(inf)/(H(sup)-H(inf));
+    endif
+  endfor
+  plot(Tr,Mr,".");
+save MagRemVar Tr Mr
+endfunction
--- Pru.m
+++ Pru.m
@@ -24,30 +24,97 @@ ymin=min(Vsal(1:1000))*1.2;
     X=Vent((i-1)*10*7557+1:(i-1)*10*7557+3779);
     Y=Vsal((i-1)*10*7557+1:(i-1)*10*7557+3779);
     I=plot(X,Y);
-    xlimits = xlim ([xmin,xmax])
-    ylimits = ylim ([ymin,ymax]);
+    xlimits = xlim([xmin,xmax]);
+    ylimits = ylim([ymin,ymax]);
     saveas(gcf,strcat('C:\Users\Gonzalo\Desktop\pruebas\medicion-',sprintf(x(k,:)),'\',sprintf(x(k,:)),'-Figure',num2str(i),'.jpg'))
   endfor
+clear X
+clear Y
 #hasta aca
 
 T=Temp(data(:,3));
 
+clear data
+
+#esto va a fijarse en el nombre del archivo cual es la frecuencia tomando los primeros numeros que aparecen
+f=str2num(strtrunc(sprintf(x(k,:)),findstr(sprintf(x(k,:)),"H")-1));
+
+# findstr(s,t) te dice en que lugares del string s esta t (un string lo toma como un vector)
+# strtrunc(s,n) te devuelve un string que tiene las primeras n simbolos del string s
+# str2num te convierte un string de numeros a numeros, si no hiciera esto, por ejemplo,
+#   tiraria error por que el 99 es un string y por lo tanto es un vector de 1x2, cuando espera un numero (1x1)
+
 #Esta parte va a hacer el grafico de magnetizacion remanente
-MrGraf2(T,Vent,Vsal,0.001);
+MrGraf2(T,Vent,Vsal,f);
 saveas(gcf,strcat('C:\Users\Gonzalo\Desktop\pruebas\medicion-',sprintf(x(k,:)),'\MagRem-',sprintf(x(k,:)),'.jpg'));
 #Guarda el grafico
+
 
 save ((strcat('C:\Users\Gonzalo\Desktop\pruebas\medicion-',sprintf(x(k,:)),'\datos-',sprintf(x(k,:)))),"t","Vent","Vsal","T");
 #Guarda las variables
 
 clear t
-clear Vent
-clear Vsal
-clear T
 clear xmax
 clear xmin
 clear ymax
 clear ymin
+clear Vent
+clear Vsal
+clear T
+#voy limpiando las variables que ya no voy a usa para q no ocupen espacio al pedo
+
+load MagRemVar
+j=1;
+i=1;
+while i<f*90
+if Mr(length(Mr)-i+1)>0
+  p(j)=Mr(length(Mr)-i+1);
+  j++;
+  i++;
+else
+  i++;
+endif
+endwhile
+os=mean(p);
+
+#opcion 1 quitarle el offset y ajustar
+#for i=1:length(Mr)
+#  if Mr(i)<0
+#    Mr0(i)=Mr(i)+os;
+#  else
+#    Mr0(i)=Mr(i)-os;
+#  endif
+#endfor
+
+#clear Mr
+
+#opcion 2 (yo uso esta ahora) ajustar usando el offset como parametro
+n=1;
+m=1;
+while Tr(n)<-25
+  if Mr(n)>0
+    TrA(m)=Tr(n);
+    MrA(m)=Mr(n);
+    m++;
+    n++;
+  else
+    n++;
+  endif
+endwhile
+clear Tr
+clear Mr
+
+MdT=@(x,A) A(1)*((A(2)-x).^(A(3)))+ A(4);
+
+fit(MdT,TrA,MrA,[1,-17,0.4,os]);
+
+
+saveas(gcf,strcat('C:\Users\Gonzalo\Desktop\pruebas\medicion-',sprintf(x(k,:)),'\Ajuste-',sprintf(x(k,:)),'.jpg'));
+#save ((strcat('C:\Users\Gonzalo\Desktop\pruebas\medicion-',sprintf(x(k,:)),'\Ajuste-',sprintf(x(k,:)))),"A");
+
+clear Mr0
+clear Tr
+
 
 endfor
 endfunction
